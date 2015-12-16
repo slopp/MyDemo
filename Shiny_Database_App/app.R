96b00ac54d7633ded0a8bac8ac3ef1334c1989fe
Sean Lopp <sean@ip-10-205-18-188.us-west-2.compute.internal>
2015-12-16 22:41:27 +0000

    Initial commit of an app that demonstrates Shiny connecting to a database through dplyr.

---
+++ Shiny_Database_App/app.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(shinydashboard)
+
+#-----------------------
+# Connect to Database
+#-----------------------
+
+
+air <- src_postgres(dbname = 'airontime', 
+                    host = 'localhost', 
+                    port = '5432', 
+                    user = 'psql_user', 
+                    password = 'ABCd4321')
+
+# Tables
+carriers <- tbl(air, "carriers")
+flights  <- tbl(air, "flights")
+
+#Carrier Names
+carrier.names <- carriers %>% select(unique(description))
+carrier.names <-collect(carrier.names) %>% arrange(description)
+
+
+#-----------------------
+# Shiny Dashboard
+#-----------------------
+
+ui <- dashboardPage(
+  dashboardHeader(title="Airline Delay Analysis"),
+  dashboardSidebar(disable=TRUE),
+  dashboardBody(
+    fluidRow(
+      box(title="Inputs", status="warning",solidHeader = TRUE,
+          selectizeInput(inputId = "carrier",label="Select Carrier",choices=carrier.names)
+      ),
+      
+      box(title="Results", status="primary", solidHeader = TRUE,
+          hr("Distribution of Delay Times"),
+          plotOutput("hist")
+      )
+      
+    ),
+    fluidRow(
+      valueBox(nrow(carrier.names), "Total Carriers", icon=icon("info-sign", lib="glyphicon"), color="teal"),
+      infoBoxOutput("carrierflights")
+    )
+  )
+)
+
+server <- function(input, output){
+  carrier.data <- reactive({
+    print(input$carrier)
+    carrier.code <- carriers %>% select(code) %>% filter(description == input$carrier)
+    carrier.code <- collect(carrier.code)
+    q <- flights %>% select(arrdelay) %>% filter(uniquecarrier == carrier.code$code)
+    collect(q)
+  })
+  
+  output$hist <- renderPlot({
+    validate(need(dim(carrier.data())[1]>0, "No flight information for this carrier"))
+    ggplot(data=carrier.data(), aes(arrdelay))+geom_histogram()+ggtitle(input$carrier)
+  })
+  
+  output$carrierflights <- renderValueBox({
+    valueBox(
+      dim(carrier.data())[1], paste("Total Flights for", input$carrier), color='teal')
+  })
+  
+}
+
+shinyApp(ui, server)
