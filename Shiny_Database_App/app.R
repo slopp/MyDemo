fea3616b8349032b318a9299cb0d48ca42d4f8ec
Sean Lopp <sean@ip-10-205-18-188.us-west-2.compute.internal>
2015-12-18 00:20:51 +0000

    Added a page that actually does some statistics...

--- Shiny_Database_App/app.R
+++ Shiny_Database_App/app.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(dplyr)
+library(dplyr)
+library(pscl)
 library(ggplot2)
 library(shinydashboard)
 library(rpivotTable)
@@ -20,9 +22,12 @@ carriers <- tbl(air, "carriers")
 flights  <- tbl(air, "flights")
 
 #Carrier Names
-carrier.names <- carriers %>% select(unique(description))
+carrier.names <- carriers %>% dplyr::select(description) %>% distinct()
 carrier.names <-collect(carrier.names) %>% arrange(description)
 
+#Cities
+cities <- flights %>% dplyr::select(origin) %>% distinct()
+cities <- collect(cities)
 
 #-----------------------
 # Shiny Dashboard
@@ -33,7 +38,8 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Explore Fields", tabName="explore"),
-      menuItem("Pivot Table", tabName="pivot")
+      menuItem("Pivot Table", tabName="pivot"),
+      menuItem("Model", tabName="model")
     ) 
   ),
   dashboardBody(
@@ -41,8 +47,8 @@ ui <- dashboardPage(
       tabItem(tabName="explore",
               fluidRow(
                 box(title="Inputs", status="warning",solidHeader = TRUE,
-                    selectizeInput(inputId = "carrier",label="Select Carrier",choices=carrier.names, selected="American Airlines Inc."),
-                    selectizeInput(inputId = "var",label="Select Variable",choices=colnames(flights), selected="arrdelay")
+                   selectizeInput(inputId = "carrier",label="Select Carrier",choices=carrier.names,selected="American Airlines Inc."),
+                   selectizeInput(inputId = "var",label="Select Variable",choices=colnames(flights),selected="arrdelay")
                 ),
                 
                 box(title="Results", status="primary", solidHeader = TRUE,
@@ -61,6 +67,26 @@ ui <- dashboardPage(
       
       tabItem(tabName="pivot", 
               rpivotTableOutput("pivot.table")
+      ),
+      
+      tabItem(tabName = "model",
+            fluidRow(
+              box(title = "Select City", status = "warning",solidHeader = TRUE,
+                 selectizeInput(inputId = "city", label="Select Departure City", choices=cities)
+              ),
+              box(title="Model Description",status="info",solidHeader = TRUE,
+                  p('A linear model will be built to predict arrival delay by airline while accounting for the other variables.'),
+                  actionButton(inputId = "runModel", label = "Run Model")
+              )
+            ),
+            
+            fluidRow(
+              box(title = "Results", status = "success", solidHeader=TRUE,width = 12,
+                  verbatimTextOutput("modelDetails"),
+                  plotOutput("delayByAirline")
+                  
+              )
+            )
       )
       
     )
@@ -69,17 +95,17 @@ ui <- dashboardPage(
 
 server <- function(input, output){
   carrier.data <- reactive({
-    validate(need(input$carrier != "", "Please Select a Carrier"))
-    validate(need(input$var != "", "Please Select a Variable"))
-    carrier.code <- carriers %>% select(code) %>% filter(description == input$carrier)
+    validate(need(input$carrier != "", "Pleasedplyr::select a Carrier"))
+    validate(need(input$var != "", "Pleasedplyr::select a Variable"))
+    carrier.code <- carriers %>%dplyr::select(code) %>% filter(description == input$carrier)
     carrier.code <- collect(carrier.code)
-    q <- flights %>% select(which(colnames(flights)==input$var)) %>% filter(uniquecarrier == carrier.code$code)
+    q <- flights %>%dplyr::select(which(colnames(flights)==input$var)) %>% filter(uniquecarrier == carrier.code$code)
     collect(q)
   })
   
   full.data <- function(){
     print("loading")
-    full.data <- flights  %>% select(year,month,dayofweek,uniquecarrier,arrdelay,depdelay,distance,origin)
+    full.data <- flights  %>%dplyr::select(year,month,dayofweek,uniquecarrier,arrdelay,depdelay,distance,origin)
     collect(full.data)
   }
   
@@ -97,6 +123,34 @@ server <- function(input, output){
     rpivotTable(full.data())
   })
   
+  run.model <- eventReactive(input$runModel, {
+    city.data <- flights %>%dplyr::select(everything()) %>% filter(arrdelay>=0) %>% filter(!is.na(arrdelay)) # %>% filter(origin == input$city)
+    city.data <- collect(city.data)
+    m <- zeroinfl(data=city.data, arrdelay ~ uniquecarrier + factor(dayofweek) + distance | 1)
+     
+  })
+  
+  parse.model <- function(m){
+    coef <- as.data.frame(m$coefficients$count)
+    coef <- cbind(coef, rownames(coef)) 
+    colnames(coef)<- c("coefficient","airline")
+    coef<-mutate(coef, carrier=grepl(pattern = "unique",coef$airline))
+    coef <- coef %>% filter(carrier) %>% dplyr::select(-carrier)%>% mutate(airline=gsub("uniquecarrier",replacement = "",x = airline))
+    carrier.data <- carriers %>% dplyr::select(everything()) %>% distinct()
+    carrier.data <- collect(carrier.data)
+    coef <- dplyr::left_join(coef,carrier.data, by=c("airline"="code"), copy=TRUE)
+    coef<- coef %>% dplyr::select(description, coefficient) %>% arrange(coefficient)
+  }
+  
+  output$modelDetails <- renderText({
+    m <- run.model()
+    as.character(m$call)
+  })
+  
+  output$delayByAirline <- renderPlot({
+    ggplot(t, aes(x=description, y=coefficient))+ geom_bar(stat="identity") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) + xlab("Airline")
+
+  })
 }
 
 shinyApp(ui, server)
